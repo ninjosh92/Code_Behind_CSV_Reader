dcb14ef8f0dfb070519d98f70747cafdc28e87f2
Matt H <matthewhoang.hoang@gmail.com>
2021-10-30 10:14:28 -0700

    Added registration and login

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -39,12 +39,19 @@ namespace Code_Behind_CSV_Reader
             t.Start();
             
         }
-        private delegate void Updater(int UI);
+        private delegate void Updater(LaunchDataSlice UI);
         private  void displayData()
         {
 
-            
 
+
+            var filePath = System.IO.Path.Combine(Environment.CurrentDirectory, "data.csv");
+            Console.WriteLine(filePath);
+            var streamReader = new StreamReader(filePath);
+            var csvReader = new CsvReader(streamReader, CultureInfo.InvariantCulture);
+            var records = csvReader.GetRecords<LaunchDataSlice>().ToList();
+
+            
             /**for (int i = 0; i < 10; i++)
             {
 
@@ -53,7 +60,7 @@ namespace Code_Behind_CSV_Reader
                 Dispatcher.BeginInvoke(DispatcherPriority.Send, uiUpdater, i);
                 Thread.Sleep(1000);
             }**/
-            
+
             /**
             foreach (LaunchDataSlice r in records)
             {
@@ -62,20 +69,39 @@ namespace Code_Behind_CSV_Reader
                 Console.WriteLine("Fuel Pressure = {0}, LOX Pressure = {1}, High Press Pressure = {2}", r.Fuel, r.Lox, r.HighPress);
                 System.Threading.Thread.Sleep(1000);
             }**/
-            
-            for (int i = 0; i < 10; i++)
+
+            for (int i = 0; i < 100; i++)
             {
                 LaunchDataSlice dataSlice = records.ElementAt(i);
                 
+
+                
                 Console.WriteLine("Fuel Pressure = {0}, LOX Pressure = {1}, High Press Pressure = {2}", dataSlice.Fuel, dataSlice.Lox, dataSlice.HighPress);
+                Updater uiUpdater = new Updater(UpdateUI);
+                //var fuel_ = dataSlice.Fuel;
+                Dispatcher.BeginInvoke(DispatcherPriority.Send, uiUpdater, dataSlice);
                 System.Threading.Thread.Sleep(1000);
             }
             
         }
 
-        private void UpdateUI(int i)
+        private void UpdateUI(LaunchDataSlice dataSlice)
         {
-            Lox_Label.Content = i;
+            double fuelReadingDouble = dataSlice.Fuel;
+            double loxReadingDouble = dataSlice.Lox;
+            double highPressReadingDouble = dataSlice.HighPress;
+
+            string fuelReadingString = fuelReadingDouble.ToString();
+            string loxReadingString = loxReadingDouble.ToString();
+            string highPressReadingString = highPressReadingDouble.ToString();
+
+            string fuelContent = string.Format("Fuel Pressure = {0}", fuelReadingString);
+            string loxContent = string.Format("LOX Pressure = {0}", loxReadingString);
+            string highPressContent = string.Format("High Press Pressure = {0}", highPressReadingString);
+
+            Fuel_Label.Content = fuelContent;
+            Lox_Label.Content = loxContent;
+            HighPress_Label.Content = highPressContent;
         }
 
         public class LaunchDataSlice
@@ -87,6 +113,20 @@ namespace Code_Behind_CSV_Reader
             [Name("PT Fuel Tank")]
             public Double Fuel { get; set; }
 
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void Exit_Button_Click(object sender, RoutedEventArgs e)
+        {
+            Application curApp = Application.Current;
+            curApp.Shutdown();
         }
     }
 }
