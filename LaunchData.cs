993a2eb2aba3bc3aca3b8682c624c18d8e4285ac
nando2nando <60991859+nando2nando@users.noreply.github.com>
2021-11-19 15:49:08 -0800

    gageValue()
    this fucn takes a string that contains the gages name with value.  it gets the float number as a string, converts and returns it as a float. that float will be used to insert it into sql launch data table

--- LaunchData.cs
+++ LaunchData.cs
@@ -4,11 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Code_Behind_CSV_Reader
 {
     class LaunchData 
     {
+
         // this function calls the open file function that returns a string with all the data
         // the func then parses the data and saves it to a list.
 
@@ -31,6 +33,7 @@ namespace Code_Behind_CSV_Reader
 
                 }
 
+                // call function to parse and save data to file in bin/debug
                 parseData(data);
 
 
@@ -71,10 +74,14 @@ namespace Code_Behind_CSV_Reader
 
             }
 
-            foreach (string item in parseData) 
+
+            /* 
+             * foreach (string item in parseData) 
             {
                 Console.WriteLine(item);
             }
+            */
+
 
             saveToFile(parseData);
         }
@@ -84,6 +91,45 @@ namespace Code_Behind_CSV_Reader
             // saved in bin/debug idk how to change the save location
             System.IO.File.WriteAllLines("LaunchData.txt", parseData);
 
+        }
+
+        public void saveToSQL() 
+        {
+           
+        }
+
+        // this fucn takes a string that contains the gages name with value. 
+        // it gets the float number as a string, converts and returns it as a float
+        public float gageValue(string dataLine)
+        {
+            float value = 0;
+            string gageValues = "";
+            // this bool makes sure that the for loop ignores the numbers in the name of the gages P4 in
+            //case
+            bool dataValues = false;
+
+
+            // iterates the string looking for the float 
+            for (int i =0; i < dataLine.Length;i++)
+            {
+                if (dataLine[i]==':') { dataValues = true; }
+                if (dataValues) 
+                {
+
+                    if (dataLine[i] >= '0' && dataLine[i] <= '9' || dataLine[i] == '.')
+                    {
+                        //adds the string char numbers to one string
+                        gageValues += dataLine[i];
+
+                    }
+                }
+                
+            }
+
+            //converts the string number into a float
+            value = float.Parse(gageValues, CultureInfo.InvariantCulture.NumberFormat);
+            
+            return value;
         }
 
        
--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -8,21 +8,31 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-
-namespace Code_Behind_CSV_Reader.Properties
-{
+namespace Code_Behind_CSV_Reader.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\PC1\\source\\repos\\Cod" +
+            "e_Behind_CSV_Reader\\LaunchData.mdf;Integrated Security=True")]
+        public string connection_string {
+            get {
+                return ((string)(this["connection_string"]));
+            }
+            set {
+                this["connection_string"] = value;
             }
         }
     }
